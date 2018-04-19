da9a99f51fe547cb1bebf4cd17f349565e936f6d
tsasselli <tsasselli@gmail.com>
2018-04-19 10:34:23 -0700

    setup for step 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import Deck from "../classes/Deck";
+import ICard from "../interfaces/ICard";
 
 @Component({
 	selector: "application",
@@ -7,8 +9,18 @@ import { Component, OnInit } from "@angular/core";
 export class AppComponent implements OnInit {
 	// write your component code here; create the properties and methods you need to get the job
 	// done as described in "app.html"; start by importing modules you need such as "./../classes/Deck"
+	public deck: Deck;
+	public drawCard: ICard;
+	public cards: ICard[];
 
-	public ngOnInit():void {
+	public ngOnInit(): void {
+		this.deck = new Deck
+		this.cards = [];
+	}
 
+	public pickCard() {
+		this.drawCard = this.deck.drawCard();
+		console.log(this.drawCard);
+		this.cards.push(this.drawCard);
 	}
 }
