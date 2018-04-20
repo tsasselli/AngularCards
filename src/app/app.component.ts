b533cc3bc06564ddb2be54231bd4f404d09f4c41
tsasselli <tsasselli@gmail.com>
2018-04-20 10:22:45 -0700

    updated notes, code cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,10 @@ import ICard from "../interfaces/ICard";
 	templateUrl: "./app.html"
 })
 export class AppComponent implements OnInit {
-	// write your component code here; create the properties and methods you need to get the job
-	// done as described in "app.html"; start by importing modules you need such as "./../classes/Deck"
 	public deck: Deck; // refrences the Deck.ts file
 	public drawCard: ICard; // ref the Icard model
-	public cards: ICard[]; // makes an array for that model so we can add the cards when build deck gets ran
+	// makes an array for that ICard model so we can add the cards when build deck gets ran
+	public cards: ICard[];
 
 	public ngOnInit(): void {
 		// triggers the build of a new deck, it gets build by the Deck.ts file's constructor runing _buildDeck
@@ -20,39 +19,42 @@ export class AppComponent implements OnInit {
 	}
 
 	public pickCard() {
-		this.drawCard = this.deck.drawCard(); // use deck to access Deck.ts and get the drawCard method
-		console.log(this.drawCard); 
-		this.cards.push(this.drawCard); // push the value of drawCard into the cards array
+		// use this.deck to access Deck.ts and get the drawCard method
+		this.drawCard = this.deck.drawCard();
+		// push the value of drawCard into the cards array
+		this.cards.push(this.drawCard);
 	}
 
 	getCardCount() {
-		// return a card count so we can use this in the HTML file to check if any are left and return a count to the user
-		return this.deck.getCardCount(); 
+		// return a card count so we can use this in the HTML file to check if 
+		// any cards left and return a count to the user
+		return this.deck.getCardCount();
 	}
 
 	returnCardToDeck(card) {
-		this.deck.returnCardToDeck(card); // gets method from Deck.ts to return the card to the deck
-		this.cards.splice(this.cards.indexOf(card), 1); // gets the index of the current card and deletes 1
+		// gets method from Deck.ts to return the card to the deck
+		this.deck.returnCardToDeck(card);
+		// gets the index of the current card(provided by *ngFor loop in html) and deletes 1
+		this.cards.splice(this.cards.indexOf(card), 1);
 	}
 
-	public getLastPickedCard(): string {
-
-		if (!this.cards.length) return; /// will return if no length left to cards.. saftey net
+	getLastPickedCard(): string {
+		if (!this.cards.length) return; /// will hit return if no length left to cards.. saftey net
 
 		const lastCardPicked = this.cards[0]; // grabs the index of last card picked in array
 
 		return lastCardPicked.rank + " of " + lastCardPicked.suit; // returns the card info
 	}
 
-	//creates a function that sets and manipulates the width of the bar based off of card count divided by total
-	cardBarWidth() {
+	//creates a function that sets and manipulates the width of the bar based off of card count divided by total(52)
+	cardCountBarWidth() {
 		const initalWidth = 170
 		return this.deck.getCardCount() / this.deck.getTotalCardCount() * initalWidth;
 	}
 
 	resetDeck() {
-		this.cards = [];
-		this.deck = new Deck;
+		this.cards = []; // emptys the local card array
+		this.deck = new Deck; //sets a new deck build
 	}
 }
 
